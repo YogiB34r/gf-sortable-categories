688aa198525b093db2628e9a1cfdf6c3e983638f
Vladimir Jankovic <v.jankovic992@gmail.com>
2018-09-04 13:22:48 +0200

    resolved js conflict with sortable categories

--- gf-sortable-categories.php
+++ gf-sortable-categories.php
@@ -26,8 +26,8 @@ function gf_sortable_categories_admin_scripts()
     if (is_admin()) {
         wp_enqueue_style('jqueri-ui-css', '//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css');
         wp_enqueue_style('gf-sortable-categories-admin-css', plugins_url() . '/gf-sortable-categories/css/gf-sortable-categories-admin.css');
-        wp_register_script('admin-js', plugins_url() . '/gf-sortable-categories/js/admin.js', array('jquery'), '', true);
-        wp_enqueue_script('admin-js');
+        wp_register_script('sortable-categories-admin-js', plugins_url() . '/gf-sortable-categories/js/sortable-categories-admin.js', array('jquery'), '', true);
+        wp_enqueue_script('sortable-categories-admin-js');
         wp_enqueue_script('jquery-ui-widget');
         wp_enqueue_script('jquery-ui-sortable');
         wp_enqueue_script('jquery-ui-accordion');
