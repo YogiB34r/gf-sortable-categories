49ce733c073a94b8164147a204f59ba5b9174aff
vjank <v.jankovic992@gmail.com>
2020-09-11 03:03:26 +0200

    Added trailing slash to category megamenu urls

--- template-parts/category-megamenu/mobile/second-level.php
+++ template-parts/category-megamenu/mobile/second-level.php
@@ -1,5 +1,5 @@
 <div class="gf-category-accordion__item gf-category-accordion__subitem mt-sm">
-    <a class="" href="<?= get_term_link((int)$cat->term_id) ?>"><?= $cat->name ?></a>
+    <a class="" href="<?=user_trailingslashit(get_term_link((int)$cat->term_id))?>"><?= $cat->name ?></a>
     <?php if (!empty(get_term_children($cat->term_id, 'product_cat'))): ?>
     <i class="gf-category-accordion__expander fas fa-plus"></i>
     <?php endif ?>
